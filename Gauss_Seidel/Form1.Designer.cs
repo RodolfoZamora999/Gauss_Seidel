b5be8f887e5a34647d4c59da4de993617b61648b
Rodolfo Zamora <rodolfozamora999@gmail.com>
2019-11-03 20:25:23 -0800

    Esta es la primera versión del programa

---
+++ Gauss_Seidel/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Gauss_Seidel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDimension = new System.Windows.Forms.Panel();
+            this.lblTolerancia = new System.Windows.Forms.Label();
+            this.lblIteraciones = new System.Windows.Forms.Label();
+            this.txtTolerancia = new System.Windows.Forms.TextBox();
+            this.txtIteracion = new System.Windows.Forms.TextBox();
+            this.btnIteracion = new System.Windows.Forms.Button();
+            this.lblDimension = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txDimension = new System.Windows.Forms.TextBox();
+            this.panelMatriz = new System.Windows.Forms.Panel();
+            this.listaResultado = new System.Windows.Forms.ListView();
+            this.panelDimension.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelDimension
+            // 
+            this.panelDimension.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panelDimension.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDimension.Controls.Add(this.lblTolerancia);
+            this.panelDimension.Controls.Add(this.lblIteraciones);
+            this.panelDimension.Controls.Add(this.txtTolerancia);
+            this.panelDimension.Controls.Add(this.btnIteracion);
+            this.panelDimension.Controls.Add(this.txtIteracion);
+            this.panelDimension.Controls.Add(this.lblDimension);
+            this.panelDimension.Controls.Add(this.btnAceptar);
+            this.panelDimension.Controls.Add(this.txDimension);
+            this.panelDimension.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelDimension.Location = new System.Drawing.Point(12, 12);
+            this.panelDimension.Name = "panelDimension";
+            this.panelDimension.Size = new System.Drawing.Size(314, 197);
+            this.panelDimension.TabIndex = 0;
+            // 
+            // lblTolerancia
+            // 
+            this.lblTolerancia.AutoSize = true;
+            this.lblTolerancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTolerancia.ForeColor = System.Drawing.Color.White;
+            this.lblTolerancia.Location = new System.Drawing.Point(89, 95);
+            this.lblTolerancia.Name = "lblTolerancia";
+            this.lblTolerancia.Size = new System.Drawing.Size(82, 20);
+            this.lblTolerancia.TabIndex = 8;
+            this.lblTolerancia.Text = "Tolerancia";
+            // 
+            // lblIteraciones
+            // 
+            this.lblIteraciones.AutoSize = true;
+            this.lblIteraciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIteraciones.ForeColor = System.Drawing.Color.White;
+            this.lblIteraciones.Location = new System.Drawing.Point(89, 57);
+            this.lblIteraciones.Name = "lblIteraciones";
+            this.lblIteraciones.Size = new System.Drawing.Size(134, 20);
+            this.lblIteraciones.TabIndex = 7;
+            this.lblIteraciones.Text = "Limite Iteraciones";
+            // 
+            // txtTolerancia
+            // 
+            this.txtTolerancia.Location = new System.Drawing.Point(18, 88);
+            this.txtTolerancia.Multiline = true;
+            this.txtTolerancia.Name = "txtTolerancia";
+            this.txtTolerancia.Size = new System.Drawing.Size(65, 32);
+            this.txtTolerancia.TabIndex = 6;
+            this.txtTolerancia.Text = "0";
+            // 
+            // txtIteracion
+            // 
+            this.txtIteracion.Location = new System.Drawing.Point(18, 50);
+            this.txtIteracion.Multiline = true;
+            this.txtIteracion.Name = "txtIteracion";
+            this.txtIteracion.Size = new System.Drawing.Size(65, 32);
+            this.txtIteracion.TabIndex = 5;
+            this.txtIteracion.Text = "0";
+            // 
+            // btnIteracion
+            // 
+            this.btnIteracion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.btnIteracion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnIteracion.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIteracion.ForeColor = System.Drawing.Color.White;
+            this.btnIteracion.Location = new System.Drawing.Point(184, 135);
+            this.btnIteracion.Name = "btnIteracion";
+            this.btnIteracion.Size = new System.Drawing.Size(116, 49);
+            this.btnIteracion.TabIndex = 4;
+            this.btnIteracion.Text = "Iniciar";
+            this.btnIteracion.UseVisualStyleBackColor = false;
+            this.btnIteracion.Click += new System.EventHandler(this.Clic_Iniciar_Iteracion);
+            // 
+            // lblDimension
+            // 
+            this.lblDimension.AutoSize = true;
+            this.lblDimension.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDimension.ForeColor = System.Drawing.Color.White;
+            this.lblDimension.Location = new System.Drawing.Point(89, 19);
+            this.lblDimension.Name = "lblDimension";
+            this.lblDimension.Size = new System.Drawing.Size(135, 20);
+            this.lblDimension.TabIndex = 3;
+            this.lblDimension.Text = "Dimensión (N x N)";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAceptar.ForeColor = System.Drawing.Color.White;
+            this.btnAceptar.Location = new System.Drawing.Point(18, 135);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(99, 38);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.Click_Crear_Matriz);
+            // 
+            // txDimension
+            // 
+            this.txDimension.Location = new System.Drawing.Point(18, 12);
+            this.txDimension.Multiline = true;
+            this.txDimension.Name = "txDimension";
+            this.txDimension.Size = new System.Drawing.Size(65, 32);
+            this.txDimension.TabIndex = 1;
+            // 
+            // panelMatriz
+            // 
+            this.panelMatriz.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panelMatriz.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMatriz.Location = new System.Drawing.Point(332, 12);
+            this.panelMatriz.Name = "panelMatriz";
+            this.panelMatriz.Size = new System.Drawing.Size(488, 197);
+            this.panelMatriz.TabIndex = 1;
+            // 
+            // listaResultado
+            // 
+            this.listaResultado.BackColor = System.Drawing.Color.White;
+            this.listaResultado.HideSelection = false;
+            this.listaResultado.Location = new System.Drawing.Point(12, 215);
+            this.listaResultado.Name = "listaResultado";
+            this.listaResultado.Size = new System.Drawing.Size(808, 427);
+            this.listaResultado.TabIndex = 2;
+            this.listaResultado.UseCompatibleStateImageBehavior = false;
+            this.listaResultado.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(836, 654);
+            this.Controls.Add(this.listaResultado);
+            this.Controls.Add(this.panelMatriz);
+            this.Controls.Add(this.panelDimension);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gauss Seidel";
+            this.panelDimension.ResumeLayout(false);
+            this.panelDimension.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDimension;
+        private System.Windows.Forms.TextBox txDimension;
+        private System.Windows.Forms.Panel panelMatriz;
+        private System.Windows.Forms.ListView listaResultado;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblDimension;
+        private System.Windows.Forms.Button btnIteracion;
+        private System.Windows.Forms.Label lblTolerancia;
+        private System.Windows.Forms.Label lblIteraciones;
+        private System.Windows.Forms.TextBox txtTolerancia;
+        private System.Windows.Forms.TextBox txtIteracion;
+    }
+}
+
---
+++ Gauss_Seidel/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gauss_Seidel
+{
+    public partial class Form1 : Form
+    {
+        private TextBox[,] matrizValores;
+        private TextBox[] matrizIgual;
+
+        //Este arreglo almacena todos los valores
+        private double[] valoresX;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.btnIteracion.Enabled = false;
+
+        }
+
+        //Evento para el botón de "Aceptar"
+        //Sirve para preparar la interfaz y todo lo necesario
+        private void Click_Crear_Matriz(object sender, EventArgs e)
+        {
+           try
+            {
+                Limpiar();
+
+                //Inicializa el vector para guardar todos los valores de las x
+                this.valoresX = new double[int.Parse(this.txDimension.Text)];
+
+                //Todos los valores se igualan a 0
+                for (int i = 0; i < this.valoresX.Length; i++)
+                    this.valoresX[i] = 0;
+                
+                Crear_Matriz(int.Parse(this.txDimension.Text));
+
+                Crear_Columnas(int.Parse(this.txDimension.Text));
+
+                this.btnIteracion.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Houston, tenemos un problema...");
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+       //Método para crear la matriz dinamica de TextBox's
+        private void Crear_Matriz(int dimension)
+        {
+            if (dimension > 1)
+            {
+                this.matrizValores = new TextBox[dimension, dimension];
+                this.matrizIgual = new TextBox[dimension];
+
+
+                for (int i = 0; i < dimension; i++)
+                {
+                    for (int j = 0; j < dimension; j++)
+                    {
+                        this.matrizValores[i, j] = new TextBox()
+                        {
+                            Size = new Size(28, 28),
+                            Location = new Point((j + 1) * 60, (i + 1) * 40),
+                            Multiline = true,
+                            TextAlign = HorizontalAlignment.Center
+                        };
+
+                        this.panelMatriz.Controls.Add(this.matrizValores[i, j]);
+
+                        Label lblX = new Label()
+                        {
+                            Size = new Size(28, 28),
+                            Location = new Point((matrizValores[i, j].Location.X) + 25, ((i + 1) * 40)),
+                            Text = "X" + (j + 1),
+                            ForeColor = Color.White,
+                            TextAlign = ContentAlignment.MiddleCenter
+                        };
+
+                        this.panelMatriz.Controls.Add(lblX);
+                    }
+
+                    //Agrega los cuadros para las "igualaciones"
+
+                    Label lblIgual = new Label()
+                    {
+                        Size = new Size(28, 28),
+                        Location = new Point((matrizValores[i, (dimension - 1)].Location.X) + 50, ((i + 1) * 40)),
+                        Text = "=",
+                        ForeColor = Color.White,
+                        TextAlign = ContentAlignment.MiddleCenter
+                    };
+
+                    this.matrizIgual[i] = new TextBox() 
+                    {
+                        Size = new Size(28, 28),
+                        Location = new Point((matrizValores[i, (dimension - 1)].Location.X) + 80, ((i + 1) * 40)),
+                        Multiline = true
+                    };
+
+                    this.panelMatriz.Controls.Add(lblIgual);
+                    this.panelMatriz.Controls.Add(this.matrizIgual[i]);
+
+                }
+
+            }
+            else
+                MessageBox.Show("Value > 1");
+        }
+
+        //Método que se encarga de crear la lista donde se desplegaran los resultados
+        private void Crear_Columnas(int variables)
+        {
+        
+            ColumnHeader[] columnas = new ColumnHeader[(variables * 2)];
+
+            this.listaResultado.Columns.Add(new ColumnHeader() {Text = "#Iteración", Width = 90 });
+
+            for(int i = 0; i < variables; i++)
+            {
+                columnas[i] = new ColumnHeader()
+                {
+                    Text = "X" + (i+1),
+                    Width = 90
+                };
+            }
+
+            byte n = 1;
+            for (int i = variables; i < (variables*2); i++)
+            {
+                columnas[i] = new ColumnHeader()
+                {
+                    Text = "Ex" + (n),
+                    Width = 90
+                };
+
+                n++;
+            }
+
+            this.listaResultado.Columns.AddRange(columnas);
+
+        }
+
+        //Método que se encarga de limpiar toda la interfaz grafica
+        private void Limpiar()
+        {
+            this.listaResultado.Columns.Clear();
+            this.listaResultado.Items.Clear();
+            this.panelMatriz.Controls.Clear();
+        }
+
+        //Método para empezar con la iteración del programa 
+        private void Clic_Iniciar_Iteracion(object sender, EventArgs e)
+        {
+
+            this.listaResultado.Items.Clear();
+
+            //Limite 
+            int limite = int.Parse(this.txtIteracion.Text);
+
+            //Toleranciad error
+            double tolerancia = double.Parse(this.txtTolerancia.Text);
+
+
+            //Dimensión de la matriz
+            int dimension = int.Parse(this.txDimension.Text);
+
+            //Array con los valores
+            double[,] matrizValoresTemp = new double[dimension, dimension];
+
+            //Esta parte de código sirve para pasar los valores de los cuadros a una matriz
+            for (int i = 0; i < dimension; i++)
+                for (int j = 0; j < dimension; j++)
+                    matrizValoresTemp[i, j] = double.Parse(this.matrizValores[i, j].Text);
+
+            //Por otra parte, aquí se almacenan los valores de las igualaciones "m = n"
+            double[] matrizIgulacionTemp = new double[dimension];
+
+            for (int i = 0; i < dimension; i++)
+                matrizIgulacionTemp[i] = double.Parse(this.matrizIgual[i].Text);
+
+
+            if (ComprobarDiagonal(matrizValoresTemp))
+            {
+                //Método que se encarga de hacer las iteraciones
+                Calcular(matrizValoresTemp, matrizIgulacionTemp, limite, tolerancia);
+            }
+            else
+                MessageBox.Show("Comprobar diagonal, ¿Quieres?");
+        }
+
+        //Método para calcular las iteraciones 
+        public void Calcular(double[,] valores, double[] igualaciones, int limite, double tolerancia)
+        {
+            //Variables que guardan los xn anteriores
+            double[] valoresXanteriores;
+            valoresXanteriores = (double[])valoresX.Clone();
+
+            //Esta parte del código sirve para imprimir los valores
+            string[] valoresImprimir = new string[ (this.valoresX.Length * 2) + 1];
+
+            //Ciclo para hacer el numero de iteraciones
+            for (int iterador = 1; iterador <= limite || limite == 0; iterador++)
+            {
+
+                valoresImprimir[0] = iterador.ToString();
+
+                //Almacenar los valores
+                for (int i = 0; i < this.valoresX.Length; i++)
+                    valoresImprimir[i+1] = Math.Round(this.valoresX[i], 4).ToString();
+
+                //Almacenar los errores
+                for (int i = this.valoresX.Length; i < valoresX.Length * 2; i++)
+                    valoresImprimir[i + 1] = Math.Round(Calcular_EX(this.valoresX[i - this.valoresX.Length],
+                        valoresXanteriores[i - this.valoresX.Length]), 4).ToString();
+
+                this.listaResultado.Items.Add(new ListViewItem(valoresImprimir));
+
+
+                //Comprobar la tolerancia
+                for (int i = 0; i < this.valoresX.Length; i++)
+                {
+                    if (Math.Round(Calcular_EX(this.valoresX[i], valoresXanteriores[i]), 4) <= tolerancia)
+                    {
+                        //Por si el error es menor o igual al establecido 
+                        MessageBox.Show("La iteración ha sido exitosa");
+                        return;
+                    }
+                }
+
+
+                //Se almacenan los valores en anteriores
+                valoresXanteriores = (double[])valoresX.Clone();
+
+                //Ciclo para recorrer los vectores
+                for (int i = 0; i < igualaciones.Length; i++)
+                {
+                    //Esta variable es temporal
+                    double valorTemp = 0;
+
+                    //Recorre un valor por cada iteración
+                    for (int j = 0; j < igualaciones.Length; j++)
+                    {
+                        if (i != j)
+                            valorTemp = valorTemp - (valores[i, j] * this.valoresX[j]);
+                    }
+
+                    valorTemp += igualaciones[i];
+
+                    //Almacena el resultado de los valores para X
+                    this.valoresX[i] = (valorTemp / valores[i, i]);
+                }
+
+            }
+
+            //Por si alcanza el limite de iteraciones
+            MessageBox.Show("La iteración ha sido exitosa por iteraciones");
+        }
+
+        public bool ComprobarDiagonal(double[,] matriz)
+        {
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(0); j++)
+                {
+                    if (matriz[i, i] < matriz[i, j])
+                    {
+                        return false;
+                    }
+
+                }
+            }
+            return true;
+        }
+
+        //Método encargado para el cálculo del error aproximado.
+        private double Calcular_EX(double xnActual, double xnAnterior)
+        {
+            return Math.Abs(((xnActual - xnAnterior) / xnActual) * 100); ;
+        }
+       
+    }
+}
